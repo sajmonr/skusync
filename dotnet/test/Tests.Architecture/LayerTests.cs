4bd5ae12af06c447a18172d6e57ba6b12595d86e
Adam Simonicek <adam@highmatch.com>
2026-05-22 17:08:23 -0400

    Renamed test project

--- dotnet/test/Tests.Architecture/LayerTests.cs
+++ dotnet/test/Tests.Architecture/LayerTests.cs
@@ -1,14 +1,15 @@
 using NetArchTest.Rules;
 using Shouldly;
 
-namespace ArchitectureTests.Layers;
+namespace Tests.Architecture.Layers;
 
 public class LayerTests : BaseTest
 {
     [Fact]
     public void IntegrationLayer_Should_NotHaveDependencyOnApplication()
     {
-        var result = Types.InAssembly(IntegrationAssembly)
+        var result = Types
+            .InAssembly(IntegrationAssembly)
             .Should()
             .NotHaveDependencyOn(ApplicationAssembly.GetName().Name)
             .GetResult();
@@ -19,7 +20,8 @@ public class LayerTests : BaseTest
     [Fact]
     public void IntegrationLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
     {
-        var result = Types.InAssembly(IntegrationAssembly)
+        var result = Types
+            .InAssembly(IntegrationAssembly)
             .Should()
             .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
             .GetResult();
@@ -30,7 +32,8 @@ public class LayerTests : BaseTest
     [Fact]
     public void IntegrationLayer_ShouldNotHaveDependencyOn_PresentationLayer()
     {
-        var result = Types.InAssembly(IntegrationAssembly)
+        var result = Types
+            .InAssembly(IntegrationAssembly)
             .Should()
             .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
             .GetResult();
@@ -41,7 +44,8 @@ public class LayerTests : BaseTest
     [Fact]
     public void ApplicationLayer_ShouldNotHaveDependencyOn_PresentationLayer()
     {
-        var result = Types.InAssembly(ApplicationAssembly)
+        var result = Types
+            .InAssembly(ApplicationAssembly)
             .Should()
             .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
             .GetResult();
@@ -52,18 +56,20 @@ public class LayerTests : BaseTest
     [Fact]
     public void InfrastructureLayer_ShouldNotHaveDependencyOn_PresentationLayer()
     {
-        var result = Types.InAssembly(InfrastructureAssembly)
+        var result = Types
+            .InAssembly(InfrastructureAssembly)
             .Should()
             .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
             .GetResult();
 
         result.IsSuccessful.ShouldBeTrue();
     }
-    
+
     [Fact]
     public void InfrastructureLayer_ShouldNotHaveDependencyOn_IntegrationLayer()
     {
-        var result = Types.InAssembly(InfrastructureAssembly)
+        var result = Types
+            .InAssembly(InfrastructureAssembly)
             .Should()
             .NotHaveDependencyOn(IntegrationAssembly.GetName().Name)
             .GetResult();
